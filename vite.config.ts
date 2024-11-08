31827f08538a924f75854782ea552be7e43e4f4a
Erik Tran <erik.tran.tt@gmail.com>
2024-11-09 01:50:10 +1100

    feat(pwa): add path aliases and resolve in vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import tsConfigs from "./tsconfig.app.json";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
@@ -7,5 +9,22 @@ export default defineConfig(({ command }) => ({
   base: "/shuttle/",
   server: {
     host: command === "serve" ? "0.0.0.0" : undefined,
+  },
+
+  /**
+   * https://stackoverflow.com/questions/77249074/how-do-i-use-typescript-path-aliases-in-vite
+   */
+  resolve: {
+    alias: {
+      ...Object.fromEntries(
+        Object.entries(tsConfigs.compilerOptions.paths)
+          .filter(([_key, value]) => {
+            return !value[0].endsWith("*");
+          })
+          .map(([key, value]) => [key, path.resolve(__dirname, value[0])]),
+      ),
+      "@": path.resolve(__dirname, "./src"),
+      "~": path.resolve(__dirname, "./src"),
+    },
   },
 }));
